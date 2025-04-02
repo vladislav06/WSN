623106ed7cafa0c6c12e528a5fb506541090f3bb
vm <mrkevinpro61@gmail.com>
2025-04-02 16:50:54 +0300

    Added new targets to flash.sh

--- PD/PD6/flash.sh
+++ PD/PD6/flash.sh
@@ -1,5 +1,5 @@
-if [ "$1" != 'transmitter' ] && [ "$1" != 'receiver' ]; then
-    echo "Target must pe specified: transmitter or receiver"
+if [ "$1" != 'sensor' ] && [ "$1" != 'relay' ] && [ "$1" != 'gateway' ]; then
+    echo "Target must pe specified: sensor, relay or gateway"
     exit
 fi
 sudo docker run --device=/dev/ttyUSB0 -v ${PWD}:/srv/repo -e MOSROOT='/srv/mans_os' -e TARG=$1 mansos-telosb bash -c "cd /srv/repo && make telosb upload"
