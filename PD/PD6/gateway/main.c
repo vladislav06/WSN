8917b54a035a590291c1a8b492851ed9b2927f8a
vm <mrkevinpro61@gmail.com>
2025-04-23 16:37:57 +0300

    Added logs

--- PD/PD6/gateway/main.c
+++ PD/PD6/gateway/main.c
@@ -21,18 +21,24 @@ void appMain(void) {
 void recvRadio(void) {
     struct Packet packet;
     int len = radioRecv(&packet, sizeof(struct Packet));
+//    PRINTF("Packet rec\n");
+
     if (len > 0) {
         // ignore invalid packets
         if (checkValidity(&packet)) {
+//            PRINTF("Packet invalid, MAGIC:%d\n", packet.magic);
             return;
         }
+
         // check device type
         if (packet.deviceType != DEVICE_TYPE_RELAY) {
+//            PRINTF("Packet invalid, devType:%d\n", packet.deviceType);
             return;
         }
 
         // Check whether this packet has already been received
         if (packetAlreadyReceived(packet.id)) {
+//            PRINTF("packetAlreadyReceived:%d|%d\n", packet.packetID, packet.deviceID);
             return;
         }
 
@@ -40,6 +46,6 @@ void recvRadio(void) {
         pushIntoCircularBuffer(packet.id);
         // send to usb
 
-        PRINTF("Data from: %d: %d", packet.deviceID, packet.payload.lightSensorValue)
+        PRINTF("Data from: %d: %d\n", packet.deviceID, packet.payload.lightSensorValue);
     }
 }
